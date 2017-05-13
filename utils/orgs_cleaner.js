e61db65ad8b4ca9edac71b1ee7e2ea250ff58b6f
kinstelli <mark@kinstelli.com>
2017-05-13 17:41:03 -0500

    removed cruft from proj data cleaner script

--- utils/orgs_cleaner.js
+++ utils/orgs_cleaner.js
@@ -8,13 +8,11 @@ const unusedFields = ['ward', 'approval_date', 'start_date__planned_','constitue
 					'duration', 'duration__months_', 'vision_2030_flagship_ministry',
 					'vision_2030_flagship_project_pr', 'implementation_status', 
 					'mtef_sector', 'work_plan_progress____', 'project_cost_yearly_breakdown__', 
-					'total_project_cost__kes_', 'implementing_agency'];
+					'implementing_agency'];
 
 let parsedJSON = JSON.parse(fs.readFileSync(fileInPath, 'utf8'));
 
 let newCollection = [];
-
-let duplicateCounter = 0;
 
 parsedJSON.features.map(function removeFields(feature){
 
@@ -37,8 +35,6 @@ parsedJSON.features.map(function removeFields(feature){
 
 //TODO: instead of eradicating data, normalize some data for filtering, but put elsewhere
 
-
-//TODO: doublecheck against duplicate coords or objects missing essential data
 function hasGeometry(newItem)
 {
 	if (newItem.geometry === null)
@@ -47,42 +43,6 @@ function hasGeometry(newItem)
 	}
 	return true;
 }
-
-function addIfUnique(newColl, newItem)
-{
-	for (var i = 0; i < newColl.length; i++)
-	{
-		//find out if this exact long/lat already exists in the array
-		if (newColl[i].geometry.coordinates[0] === newItem.geometry.coordinates[0]
-			&& newColl[i].geometry.coordinates[1] === newItem.geometry.coordinates[1])
-		{
-			console.log('matched:');
-			//then, decide what to keep...
-			//if the existing record has no data, and the new one *does*, then overwrite old record
-			if (newColl[i].properties.project_title === null && newItem.properties.project_title !== null)
-			{
-				console.log('Existing item has no title, new item does.');
-				newColl[i] = newItem;
-				newColl[i].properties.projs_at_loc++;
-				return;
-			}
-			//if the existing record has proj name/desc data, and this one doesn't, then don't add it, but inc count?
-			else if (newColl[i].properties.project_title !== null && newItem.properties.project_title === null)
-			{
-				console.log('Existing item has title, new item does not.');
-				newColl[i].properties.projs_at_loc++;
-				return;
-			}
-			console.log('Both records have data: \nold:', newColl[i].properties.project_title, '\nnew:', newItem.properties.project_title);
-			//if they both have data, then proceed to add the record
-			newItem.projs_at_loc = 1;
-		}
-	}
-
-	//TODO: this is a potentially ugly side-effect, don't rely on array ref here
-	newColl.push(newItem);
-}
-
 
 //reassemble to original object
 parsedJSON.features = newCollection;
